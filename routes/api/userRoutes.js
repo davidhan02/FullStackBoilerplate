fe6d5ec292544e320a93a0e546c3665017ce30fd
David Han <davidhan.5.25@gmail.com>
2019-04-21 19:15:04 -0700

    Added login functionality

--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -13,29 +13,57 @@ router.get('/test', (req, res) =>
   })
 );
 
-// @route   GET api/users/test
-// @desc    Test users route
+// @route   POST api/users/register
+// @desc    Register a user
 // @access  Public
-router.post('/register', async (req, res, next) => {
-  const user = await User.findOne({ email: req.body.email });
-  if (user) {
-    res.status(400).json({ email: 'Email already exists' });
-  } else {
-    const newUser = new User({
-      name: req.body.name,
-      email: req.body.email,
-      password: req.body.password
-    });
+router.post('/register', (req, res, next) => {
+  const { name, email, password } = req.body;
 
-    bcrypt.genSalt(10, (err, salt) => {
-      bcrypt.hash(newUser.password, salt, async (err, hash) => {
-        if (err) throw err;
-        newUser.password = hash;
-        const addedUser = await newUser.save();
-        res.json(addedUser);
-      });
+  User.findOne({ email })
+    .then(user => {
+      if (user) {
+        return res.status(400).json({ email: 'Email already exits' });
+      } else {
+        const newUser = new User({
+          name,
+          email,
+          password
+        });
+
+        bcrypt.genSalt(10, (err, salt) => {
+          if (err) throw err;
+          bcrypt.hash(newUser.password, salt, (err, hash) => {
+            if (err) throw err;
+            newUser.password = hash;
+            newUser
+              .save()
+              .then(user => res.json(user))
+              .catch(err => console.log(err));
+          });
+        });
+      }
+    })
+    .catch(err => console.log(err));
+});
+
+// @route   POST api/users/login
+// @desc    Login a user / Returning auth token
+// @access  Public
+router.post('/login', async (req, res) => {
+  const { email, password } = req.body;
+
+  const user = await User.findOne({ email }).then(user => {
+    if (!user) {
+      return res.status(404).json({ email: 'User email not found' });
+    }
+
+    bcrypt.compare(password, user.password).then(isMatch => {
+      if (isMatch) {
+        return res.json({ msg: 'Success' });
+      }
+      return res.status(400).json({ password: 'Password incorrect' });
     });
-  }
+  });
 });
 
 module.exports = router;
