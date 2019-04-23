e64a8af19a6e66f157747d49a453a3a319baff09
David Han <davidhan.5.25@gmail.com>
2019-04-22 18:39:53 -0700

    added redux-thunk and axios, refactor slightly

--- client/src/index.js
+++ client/src/index.js
@@ -5,8 +5,7 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 
 import App from './components/App';
-
-import store from './store';
+import store from './utils/store';
 
 ReactDOM.render(
   <Provider store={store}>
