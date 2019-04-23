bcfd4d3753534126c2e30200a62435b74b069b2b
David Han <davidhan.5.25@gmail.com>
2019-04-22 17:01:25 -0700

    Start Redux setup into React

--- client/src/index.js
+++ client/src/index.js
@@ -4,4 +4,11 @@ import { Provider } from 'react-redux';
 
 import App from './components/App';
 
-ReactDOM.render(<App />, document.querySelector('#root'));
+import store from './store';
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.querySelector('#root')
+);
