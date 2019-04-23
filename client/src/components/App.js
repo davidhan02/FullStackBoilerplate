9750fe1c7b92401d18455476a55ab45209797040
David Han <davidhan.5.25@gmail.com>
2019-04-22 18:21:47 -0700

    Added bootstrap to client for grid use and setup router

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,11 +1,28 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
+import { Route, Switch, BrowserRouter as Router } from 'react-router-dom';
+
+import Header from './layout/Header';
+const Dashboard = () => <h2>Dashboard</h2>;
+const Login = () => <h2>Login</h2>;
+const Register = () => <h2>Register</h2>;
+const Landing = () => <h2>Landing</h2>;
 
 class App extends Component {
   render() {
     return (
-      <div>
-        <h1>App</h1>
-      </div>
+      <Router>
+        <Fragment>
+          <Header />
+          <div className="container">
+            <Switch>
+              <Route exact path="/" component={Landing} />
+              <Route exact path="/login" component={Login} />
+              <Route exact path="/register" component={Register} />
+              <Route exact path="/dashboard" component={Dashboard} />
+            </Switch>
+          </div>
+        </Fragment>
+      </Router>
     );
   }
 }
---
+++ client/src/components/layout/Header.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+
+class Header extends Component {
+  render() {
+    return (
+      <nav>
+        <div className="nav container">
+          <Link className="nav-brand" to="/">
+            Company Logo
+          </Link>
+          <ul className="nav-list">
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/login">Login</Link>
+            </li>
+            <li>
+              <Link to="/register">Register</Link>
+            </li>
+            <li>
+              <Link to="/dashboard">Dashboard</Link>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    );
+  }
+}
+
+export default Header;
